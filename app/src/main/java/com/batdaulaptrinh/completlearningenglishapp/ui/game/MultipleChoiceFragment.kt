3eeff72d65c902df029b4c9801cbbd7c36673b55
19522531 <19522531@gm.uit.edu.vn>
2021-12-01 16:38:32 +0700

    Update

--- app/src/main/java/com/batdaulaptrinh/completlearningenglishapp/ui/game/MultipleChoiceFragment.kt
+++ app/src/main/java/com/batdaulaptrinh/completlearningenglishapp/ui/game/MultipleChoiceFragment.kt
@@ -2,13 +2,16 @@ package com.batdaulaptrinh.completlearningenglishapp.ui.game
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.service.media.MediaBrowserService
 import android.util.Base64
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isInvisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -24,11 +27,11 @@ import kotlin.random.Random
 class MultipleChoiceFragment : Fragment() {
     companion object {
         val KEY_AGRS_SET = "WORD_SET"
-        var count = 0
+        var count = 1
         var correct = 0
         var incorrect = 0
         var check = true
-        var randomValues: List<Int> = List(4) { Random.nextInt(0, 20) }
+        var randomValues: List<Int> = List(4) { Random.nextInt(0, 3) }
         var entext = ""
         lateinit var listword: List<Word>
 
@@ -55,18 +58,17 @@ class MultipleChoiceFragment : Fragment() {
 
 
 
-        if (count == 0)
+        if (count == 1)
         {
             listword = listword2.asSequence().shuffled().take(20).toList()
-            randomValues = List(4) { Random.nextInt(0, 20) }
-            var decodedImageA = decode( randomValues[0], listword)
-            binding.answerATxt.setImageBitmap(decodedImageA)
-            var decodedImageB = decode(randomValues[1], listword)
-            binding.answerBTxt.setImageBitmap(decodedImageB)
-            var decodedImageC = decode(randomValues[2], listword)
-            binding.answerCTxt.setImageBitmap(decodedImageC)
-            var decodedImageD = decode(randomValues[3], listword)
-            binding.answerDTxt.setImageBitmap(decodedImageD)
+            var decodedImageA = decode( 0, listword)
+            binding.answerAImg.setImageBitmap(decodedImageA)
+            var decodedImageB = decode(1, listword)
+            binding.answerBImg.setImageBitmap(decodedImageB)
+            var decodedImageC = decode(2, listword)
+            binding.answerCImg.setImageBitmap(decodedImageC)
+            var decodedImageD = decode(3, listword)
+            binding.answerDImg.setImageBitmap(decodedImageD)
 
             entext  = listword[randomValues.asSequence().shuffled().take(1).toList()[0]].en_word
             binding.enWordTxt.setText(entext)
@@ -74,40 +76,290 @@ class MultipleChoiceFragment : Fragment() {
             binding.numberCorrectAnswerTxt.setText("Correct: "+ correct.toString())
             binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
             binding.progressTxt.setText(count.toString()+"/20")
+
         }
         binding.checkBtn.setOnClickListener()
         {
             count +=1
-            randomValues = List(4) { Random.nextInt(0, 20) }
             listword = listword2.asSequence().shuffled().take(20).toList()
-            val decodedImageA = decode( randomValues[0], listword)
-            binding.answerATxt.setImageBitmap(decodedImageA)
-            val decodedImageB = decode(randomValues[1], listword)
-            binding.answerBTxt.setImageBitmap(decodedImageB)
-            val decodedImageC = decode(randomValues[2], listword)
-            binding.answerCTxt.setImageBitmap(decodedImageC)
-            val decodedImageD = decode(randomValues[3], listword)
-            binding.answerDTxt.setImageBitmap(decodedImageD)
+            val decodedImageA = decode( 0, listword)
+            binding.answerAImg.setImageBitmap(decodedImageA)
+            val decodedImageB = decode(1, listword)
+            binding.answerBImg.setImageBitmap(decodedImageB)
+            val decodedImageC = decode(2, listword)
+            binding.answerCImg.setImageBitmap(decodedImageC)
+            val decodedImageD = decode(3, listword)
+            binding.answerDImg.setImageBitmap(decodedImageD)
             entext  = listword[Companion.randomValues.asSequence().shuffled().take(1).toList()[0]].en_word
             binding.enWordTxt.setText(entext)
             Log.e("ENTEXT", entext)
+            if (correct + incorrect != count-1)
+            {
+                incorrect+=1
+            }
             binding.numberCorrectAnswerTxt.setText("Correct: "+ correct.toString())
             binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
             binding.progressTxt.setText(count.toString()+"/20")
+            binding.ansTxt.setText("")
             check = true
+            binding.answerATxt.setBackgroundResource(R.drawable.boder_image_none)
+            binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_none)
+            binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_none)
+            binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_none)
         }
-        if (check)
+
+        binding.answerATxt.setOnClickListener()
         {
-            binding.answerATxt.setOnClickListener()
-            {
-                if (listword[randomValues[0]].en_word == entext) {
-                    correct +=1
+            if (check)
+                {   binding.checkBtn.visibility
+                    if (listword[0].en_word == entext) {
+                        correct += 1
+                        binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image)
+                        binding.ansTxt.setText("Yeeh correct")
+
+                    } else {
+                        incorrect += 1
+                        binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                        if (listword[1].en_word==entext)
+                        {
+                            binding.ansTxt.setText("Oh no incorrect    answer is B")
+                            binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_red)
+                        }
+                        else if (listword[2].en_word==entext)
+                        {
+                            binding.ansTxt.setText("Oh no incorrect    answer is C")
+                            binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_red)
+                        }
+                        else if (listword[3].en_word==entext)
+                        {
+                            binding.ansTxt.setText("Oh no incorrect  answer is D")
+                            binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_red)
+                        }
+
+                    }
+                    check = false
+                }
+        }
+        binding.answerBTxt.setOnClickListener()
+        {
+            if (check) {
+                if (listword[1].en_word == entext) {
+                    correct += 1
                     binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.ansTxt.setText("Yeeh correct")
+                    binding.answerBTxt.setBackgroundResource(R.drawable.boder_image)
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[0].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is A")
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[2].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is C")
+                        binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[3].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect  answer is D")
+                        binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                }
+                check = false
+            }
+
+        }
+        binding.answerCTxt.setOnClickListener()
+        {
+            if (check) {
+                if (listword[2].en_word == entext) {
+                    correct += 1
+                    binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.ansTxt.setText("Yeeh correct")
+                    binding.answerCTxt.setBackgroundResource(R.drawable.boder_image)
+
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[0].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is A")
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[1].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is B")
+                        binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[3].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is D")
+                        binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                }
+                check = false
+            }
+        }
+        binding.answerDTxt.setOnClickListener()
+        {
+            if (check) {
+                if (listword[3].en_word == entext) {
+                    correct += 1
+                    binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.ansTxt.setText("Yeeh correct")
+                    binding.answerDTxt.setBackgroundResource(R.drawable.boder_image)
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[0].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is A")
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[1].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is B")
+                        binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[2].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is C")
+                        binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                }
+                check = false
+            }
+        }
+        binding.answerAImg.setOnClickListener()
+        {
+            if (check)
+            {   binding.checkBtn.visibility
+                if (listword[0].en_word == entext) {
+                    correct += 1
+                    binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.answerATxt.setBackgroundResource(R.drawable.boder_image)
+                    binding.ansTxt.setText("Yeeh correct")
+
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[1].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect    answer is B")
+                        binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[2].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect    answer is C")
+                        binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[3].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect  answer is D")
+                        binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
 
                 }
                 check = false
             }
         }
+        binding.answerBImg.setOnClickListener()
+        {
+            if (check) {
+                if (listword[1].en_word == entext) {
+                    correct += 1
+                    binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.ansTxt.setText("Yeeh correct")
+                    binding.answerBTxt.setBackgroundResource(R.drawable.boder_image)
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[0].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is A")
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[2].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is C")
+                        binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[3].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect  answer is D")
+                        binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                }
+                check = false
+            }
+
+        }
+        binding.answerCImg.setOnClickListener()
+        {
+            if (check) {
+                if (listword[2].en_word == entext) {
+                    correct += 1
+                    binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.ansTxt.setText("Yeeh correct")
+                    binding.answerCTxt.setBackgroundResource(R.drawable.boder_image)
+
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[0].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is A")
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[1].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is B")
+                        binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[3].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is D")
+                        binding.answerDTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                }
+                check = false
+            }
+        }
+        binding.answerDImg.setOnClickListener()
+        {
+            if (check) {
+                if (listword[3].en_word == entext) {
+                    correct += 1
+                    binding.numberCorrectAnswerTxt.setText("Correct: " + correct.toString())
+                    binding.ansTxt.setText("Yeeh correct")
+                    binding.answerDTxt.setBackgroundResource(R.drawable.boder_image)
+                } else {
+                    incorrect += 1
+                    binding.numberIncorrectAnswerTxt.setText("Incorrect: " + incorrect.toString())
+                    if (listword[0].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is A")
+                        binding.answerATxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[1].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is B")
+                        binding.answerBTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                    else if (listword[2].en_word==entext)
+                    {
+                        binding.ansTxt.setText("Oh no incorrect   answer is C")
+                        binding.answerCTxt.setBackgroundResource(R.drawable.boder_image_red)
+                    }
+                }
+                check = false
+            }
+        }
+
+
         binding.backwardImg.setOnClickListener{
             findNavController().popBackStack()
         }
